157fbdaaa11b977f302b0a9d2ef5d12940160c25
VishnuPriyaKuppurao <vishnupriyakuppurao@gmail.com>
2025-07-27 20:39:46 +0530

    Added alerts file

---
+++ alerts.java
@@ -0,0 +1,30 @@
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class alerts {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		WebDriver driver=new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		driver.manage().window().maximize();
+		driver.findElement(By.name("enter-name")).sendKeys("Riyanth");
+		driver.findElement(By.id("alertbtn")).click();
+		Thread.sleep(1000);
+		System.out.println(driver.switchTo().alert().getText());
+		driver.switchTo().alert().accept();
+		Thread.sleep(1000);
+		driver.findElement(By.id("confirmbtn")).click();
+		System.out.println(driver.switchTo().alert().getText());
+		Thread.sleep(1000);
+		driver.switchTo().alert().dismiss();
+		driver.close();
+		
+
+	}
+
+}
